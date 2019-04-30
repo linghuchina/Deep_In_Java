02a31158240b5d89966ebed883e2bdcf040626a7
linghuchina <1500479344@qq.com>
2019-04-30 22:57:21 +0800

    java线程框架

--- Deep_In_Java/src/main/java/com/alibaba/java/CollectionFramework/SynchronizedMap.java
+++ Deep_In_Java/src/main/java/com/alibaba/java/CollectionFramework/SynchronizedMap.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 早期的并发实现
+ */
 public class SynchronizedMap {
     public static void main(String[] args) {
         //看源码的时候，如果return中出现view 的时候，说明返回的一定是只读的
