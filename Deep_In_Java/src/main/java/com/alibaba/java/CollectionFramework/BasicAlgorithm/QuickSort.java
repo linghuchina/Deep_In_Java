bb343cac5b987b4a823305a68334e57c2df51f5c
linghuchina <42994244+linghuchina@users.noreply.github.com>
2019-07-01 06:26:46 +0800

    QuickSort
    QuickSort

--- Deep_In_Java/src/main/java/com/alibaba/java/CollectionFramework/BasicAlgorithm/QuickSort.java
+++ Deep_In_Java/src/main/java/com/alibaba/java/CollectionFramework/BasicAlgorithm/QuickSort.java
@@ -3,13 +3,28 @@ package com.alibaba.java.CollectionFramework.BasicAlgorithm;
 /**
  * 快排：
  * 1： 先找到一个数组的最后一位，以此为比较轴
- * 2： 根据轴做一个分区
- * @param <T>
+         * 2： 根据轴做一个分区
+         * @param <T>
  */
 public class QuickSort<T extends Comparable<T>> implements Sort{
     @Override
     public void Sort(Comparable[] values) {
         int size = values.length;
+        Sort(values,0,size-1);
+    }
+    public void Sort(Comparable[] values,int lowP , int highP) {
+        int size = values.length;
+        int high = highP;
+        int low = lowP;
+        int  position = partition((T[]) values,low,high);
+        System.out.println(position);
+        if(position-1>lowP){
+            Sort(values,lowP,position-1);
+        }
+        if (highP>position+1){
+            Sort(values,position+1,highP);
+        }
+
     }
 
     /**
@@ -20,6 +35,24 @@ public class QuickSort<T extends Comparable<T>> implements Sort{
      * @return 分区索引
      */
     int partition(T[] values, int low,int high){
-        return 0;
+        T position  =  values[high];
+        int i = low;
+        int j = high;
+        while(i<j){
+            while(position.compareTo(values[i])>=1&&j>i) i++;
+            while(position.compareTo(values[j])<=1&&j>i) j--;
+            if(i<j){
+                T temp = values[i];
+                values[i] = values[j];
+                values[j]=temp;
+            }
+        }
+         return  i;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1,4,2,5,10,0};
+        Sort sort = new QuickSort();
+        sort.Sort(arr);
     }
 }
